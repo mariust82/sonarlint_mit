fd05a7b1ef475d22cd66845053051ebc8ec4b744
Marius Toader <marius.t@gtmarketing.ro>
2023-10-23 18:05:20 +0300

    +Test PHP-PSR2 quality profile checks

--- CategoriesController.php
+++ CategoriesController.php
@@ -2,10 +2,42 @@
 require_once 'application/controllers/AbstractLoggedInController.php';
 require_once 'application/models/dao/AcademyCategories.php';
 
-class CategoriesController extends AbstractLoggedInController {
+class CategoriesController extends AbstractLoggedInController
+{
 
-    protected function service() {
+    protected function service()
+    {
         $categories = new AcademyCategories();
         $this->response->attributes("categories", $categories->getAll());
         $this->response->attributes("totalGuides", $categories->getCategoryTotalGuides());
     }
+
+    protected function setFields() : void
+    {
+        $fields = $this->query->fields();
+        $fields
+            ->add("DISTINCT games.id")
+            ->add("games.name","game_name")
+            ->add("games.score")
+            ->add("game_manufacturers.name")
+            ->add("COALESCE(gpl.played_times, 0)", "times_played");
+        if ($this->filter->getDateLaunched()) {
+            $fields->add("games.date_launched");
+        }
+        //games without free play
+        if ($this->filter->getGamesWithoutFreePlay()) {
+            $fields->add("IF(gpp.isMobile IN (".($this->filter->getViewport() ? 1 : 0).", '2'), 0, 1)", "no_free_play");
+        }
+    }
+
+    protected function constantsTest()
+    {
+        define( 'CONSTANT_VALUE', 'old value' );
+        define( 'SCRIPT_DEBUG', 1 );
+
+        // Noncompliant, tries to redefine constant defined 2 lines above
+        define( 'CONSTANT_VALUE', 'intended value' );
+        echo CONSTANT_VALUE; // output: 'old value'
+
+    }
+}
